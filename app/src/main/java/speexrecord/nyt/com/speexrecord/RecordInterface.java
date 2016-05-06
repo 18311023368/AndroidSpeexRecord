fddc503a56fe05e900749d93fcc8ea501e9a284b
nieyt <nieyt@zgxcw.com>
2016-05-06 14:56:33 +0800

    录音接口

---
+++ app/src/main/java/speexrecord/nyt/com/speexrecord/RecordInterface.java
@@ -0,0 +1,12 @@
+package speexrecord.nyt.com.speexrecord;
+
+/**
+ * @作者：聂钰谭
+ * @创建日期： 2016/5/6 14:51
+ * @实现功能：
+ * @更改日志：
+ */
+public interface RecordInterface {
+    void  startRecord(boolean isStart);//开始录音
+    void stopRecord(boolean isStart);//停止录音
+}
